2f6c887da5dba9afab44335f9fdc7652ab5615e7
Viktor Shuntikov <schuntikov.viktor@yandex.ru>
2025-08-09 18:27:37 +0700

    header section implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-import Header from './Header.js';
-import TabsCollection from './Tabs.js';
-import VideoPlayerCollection from './VideoPlayer.js';
-import ExpandableContentCollection from './ExpandableContent.js';
-import InputMaskCollection from './InputMask.js';
-import SelectCollection from './Select.js';
+import Header from './Header.js'
 
-new Header();
-new TabsCollection();
-new VideoPlayerCollection();
-new ExpandableContentCollection();
-new InputMaskCollection();
-new SelectCollection();
+new Header()
