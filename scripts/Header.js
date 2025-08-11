a20f10729338b75231dc4d7caea4688fa1133339
Viktor Shuntikov <schuntikov.viktor@yandex.ru>
2025-08-11 20:41:11 +0700

    fixed not closiong mobile menu onClick

--- scripts/Header.js
+++ scripts/Header.js
@@ -2,6 +2,7 @@ class Header {
   selectors = {
     root: '[data-js-header]',
     overlay: '[data-js-header-overlay]',
+    link: '[data-js-header-link]',
     burgerButton: '[data-js-header-burger-button]'
   }
 
@@ -12,6 +13,7 @@ class Header {
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root)
+    this.linkElements = this.rootElement.querySelectorAll(this.selectors.link)
     this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
     this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton)
     this.bindEvents()
@@ -23,8 +25,18 @@ class Header {
     document.documentElement.classList.toggle(this.stateClasses.isLock)
   }
 
+  onClickMenuLink = () => {
+    this.overlayElement.classList.remove(this.stateClasses.isActive)
+    this.burgerButtonElement.classList.remove(this.stateClasses.isActive)
+    document.documentElement.classList.remove(this.stateClasses.isLock)
+  }
+
   bindEvents() {
     this.burgerButtonElement.addEventListener('click', this.onClickButtonBurger)
+
+    this.linkElements.forEach(link => {
+      link.addEventListener('click', this.onClickMenuLink)
+    })
   }
 }
 
